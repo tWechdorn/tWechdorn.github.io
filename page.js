394f04e8d77cf153bc558c1d23528f3dc454fc68
Thomas Wechdorn <thomas.wechdorn@allianz.at>
2021-04-18 11:34:30 +0200

    update to new version

--- page.js
+++ page.js
@@ -1,4 +1,5 @@
 var count = 1;
+var appleCut = false;
 
 function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
@@ -13,21 +14,33 @@ function drop(ev){
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
 }
+function cutApple(){
+  if(!appleCut){
+    $('#canvas').prepend(createFraction(1,2));
+    var draw = SVG().addTo('#canvas').size(200, 130);
+    let img = draw.image('apple.png').size(200,130);
+    $('#canvas').append(createFraction(1,2));
+    appleCut = true;
+    $('.hidden').css("display","inline");
+  }
+}
 
 function cut(){
     for(i=0; i < count; i++)
     {
-      var draw = SVG().addTo('#canvas').size(500, 130);
-      let img = draw.image('apple.png').size(500,130);
+      var draw = SVG().addTo('#canvas').size(200, 130);
+      let img = draw.image('apple.png').size(200,130);
     }
     count *= 2;
 }
 
 window.onload= function(){
-var canvas = document.getElementById("canvas_1");
-var distance = 10;
-var draw = SVG().addTo('#canvas').size(500, 130);
-var svg = $("svg").attr("onclick", "cut()").attr("id", "svg")
+var draw = SVG().addTo('#canvas').size(200, 130);
+var svg = $("svg").attr("onclick", "cutApple()").attr("id", "svg")
 var image = draw.image('apple.png');
-image.size(500,130);
+image.size(200,130);
+}
+
+function createFraction( numerator, denominator){
+  return "<div class=\"fraction\"><span>"+numerator+"</span><span class=\"denominator\">"+denominator+"</span></div>"
 }
